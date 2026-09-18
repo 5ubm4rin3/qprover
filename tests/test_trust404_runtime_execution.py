9b053180d4da582b9f154d462294c48a9b411433
5ubm4rin3 <136168259+5ubm4rin3@users.noreply.github.com>
2026-09-18 14:33:05 +0900

    style: sort Track04 persistent runtime imports

--- tests/test_trust404_runtime_execution.py
+++ tests/test_trust404_runtime_execution.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
 
-from qprover.trust404_runtime import RuntimeCall, Track04Runtime
 from web3 import Web3
+
+from qprover.trust404_runtime import RuntimeCall, Track04Runtime
 
 
 CONTROLLER = "0x00000000000000000000000000000000000000c0"
