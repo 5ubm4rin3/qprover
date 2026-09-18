075231f147e32228aefdd91788ba8dcc3ecb1d7d
5ubm4rin3 <136168259+5ubm4rin3@users.noreply.github.com>
2026-09-18 16:14:22 +0900

    test: cover generic Track04 callback dispatch

--- trust404/harness/test/SearchAttacker.t.sol
+++ trust404/harness/test/SearchAttacker.t.sol
@@ -12,6 +12,21 @@ contract SenderRecorder {
     }
 }
 
+contract CallbackSink {
+    uint256 public hits;
+
+    function mark() external {
+        hits += 1;
+    }
+}
+
+contract CallbackTrigger {
+    function trigger() external {
+        (bool ok,) = msg.sender.call("");
+        require(ok, "callback");
+    }
+}
+
 contract SearchAttackerTest is Test {
     function test_targetObservesPersistentSearchAttackerAsSender() public {
         SenderRecorder target = new SenderRecorder();
@@ -25,5 +40,27 @@ contract SearchAttackerTest is Test {
 
         assertEq(target.seenSender(), address(attacker));
         assertTrue(target.seenSender() != address(this));
+    }
+
+    function test_receiveCallbackExecutesDifferentConfiguredAction() public {
+        SearchAttacker attacker = new SearchAttacker();
+        CallbackTrigger trigger = new CallbackTrigger();
+        CallbackSink sink = new CallbackSink();
+
+        address[] memory targets = new address[](1);
+        uint256[] memory values = new uint256[](1);
+        bytes[] memory data = new bytes[](1);
+        targets[0] = address(sink);
+        values[0] = 0;
+        data[0] = abi.encodeWithSignature("mark()");
+
+        attacker.configureCallback(targets, values, data, 1);
+        attacker.execute(
+            address(trigger),
+            0,
+            abi.encodeWithSignature("trigger()")
+        );
+
+        assertEq(sink.hits(), 1);
     }
 }
